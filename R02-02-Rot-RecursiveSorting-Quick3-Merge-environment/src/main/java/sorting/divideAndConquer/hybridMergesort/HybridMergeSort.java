958614555009a7f0393897ced6b34f205262d8ef
Suelen Felix <54537490+SuelenFelix@users.noreply.github.com>
2023-04-17 20:27:16 -0300

    Add files via upload

--- R02-02-Rot-RecursiveSorting-Quick3-Merge-environment/src/main/java/sorting/divideAndConquer/hybridMergesort/HybridMergeSort.java
+++ R02-02-Rot-RecursiveSorting-Quick3-Merge-environment/src/main/java/sorting/divideAndConquer/hybridMergesort/HybridMergeSort.java
@@ -1,6 +1,9 @@
 package sorting.divideAndConquer.hybridMergesort;
 
 import sorting.AbstractSorting;
+import util.Util;
+
+import java.util.Arrays;
 
 /**
  * A classe HybridMergeSort representa a implementação de uma variação do
@@ -8,29 +11,82 @@ import sorting.AbstractSorting;
  * seguinte forma: o MergeSort é aplicado a entradas maiores a um determinado
  * limite. Caso a entrada tenha tamanho menor ou igual ao limite o algoritmo usa
  * o InsertionSort.
- * 
+ * <p>
  * A implementação híbrida deve considerar os seguintes detalhes:
  * - Ter contadores das quantidades de MergeSorts e InsertionSorts aplicados, de forma
- *   que essa informação possa ser capturada pelo teste.
+ * que essa informação possa ser capturada pelo teste.
  * - A cada chamado do método de sort(T[] array) esses contadores são resetados. E a cada chamada
- *   interna de um merge ou insertion, os contadores MERGESORT_APPLICATIONS e
- *   INSERTIONSORT_APPLICATIONS são incrementados.
+ * interna de um merge ou insertion, os contadores MERGESORT_APPLICATIONS e
+ * INSERTIONSORT_APPLICATIONS são incrementados.
  * - O InsertionSort utilizado no algoritmo híbrido deve ser in-place.
  */
 public class HybridMergeSort<T extends Comparable<T>> extends
-		AbstractSorting<T> {
+        AbstractSorting<T> {
 
-	/**
-	 * For inputs with size less or equal to this value, the insertionsort
-	 * algorithm will be used instead of the mergesort.
-	 */
-	public static final int SIZE_LIMIT = 4;
+    /**
+     * For inputs with size less or equal to this value, the insertionsort
+     * algorithm will be used instead of the mergesort.
+     */
+    public static final int SIZE_LIMIT = 4;
 
-	protected static int MERGESORT_APPLICATIONS = 0;
-	protected static int INSERTIONSORT_APPLICATIONS = 0;
+    protected static int MERGESORT_APPLICATIONS = 0;
+    protected static int INSERTIONSORT_APPLICATIONS = 0;
 
-	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    public void sort(T[] array, int leftIndex, int rightIndex) {
+        MERGESORT_APPLICATIONS = 0;
+        INSERTIONSORT_APPLICATIONS = 0;
+        hybridMergeSort(array, leftIndex, rightIndex);
+    }
+
+    private void hybridMergeSort(T[] array, int start, int end) {
+        if (array.length != 0 || array.length != 1) {
+
+            int mid = (start + end) / 2;
+
+            if (end - start <= SIZE_LIMIT) {
+                insertionSort(array, start, end);
+            } else {
+                hybridMergeSort(array, start, mid);
+                hybridMergeSort(array, mid + 1, end);
+                merge(array, start, end);
+            }
+        }
+    }
+
+    private void insertionSort(T[] array, int start, int end) {
+        for (int i = start + 1; i < end + 1; i++) {
+            for (int j = i; j > start; j--) {
+                boolean isCurrentSmallerThanPrevious = array[j].compareTo(array[j - 1]) < 0;
+                if (isCurrentSmallerThanPrevious) {
+                    Util.swap(array, j - 1, j);
+                }
+            }
+        }
+        INSERTIONSORT_APPLICATIONS++;
+    }
+
+    private void merge(T[] array, int start, int end) {
+        T[] helper = Arrays.copyOf(array, array.length);
+
+        int i = start;
+        int mid = (start + end) / 2;
+        int j = mid + 1;
+        int k = start;
+        while (i <= mid && j <= end) {
+            boolean isStartElementSmallerThanEndElement = helper[i].compareTo(helper[j]) <= 0;
+            if (isStartElementSmallerThanEndElement) {
+                array[k] = helper[i++];
+            } else {
+                array[k] = helper[j++];
+            }
+            k++;
+        }
+        while (i <= mid) {
+            array[k++] = helper[i++];
+        }
+        while (j <= end) {
+            array[k++] = helper[j++];
+        }
+        MERGESORT_APPLICATIONS++;
+    }
 }
